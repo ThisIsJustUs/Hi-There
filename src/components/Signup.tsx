197b24699f70dd258b0b6a04c321069d00b28e3d
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2020-09-14 23:35:05 +0200

    Made input to email field

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -44,7 +44,7 @@ export const Signup: React.FC = () => {
                                 <input
                                     className='w-3/5 mr-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                                     id='inline-full-name'
-                                    type='text'
+                                    type='email'
                                     name='email'
                                     placeholder='Email'
                                     value={email}
