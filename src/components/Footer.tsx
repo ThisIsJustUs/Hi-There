eca5cde120ba372107c88b68984af58ee2365a3b
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2020-09-17 15:30:53 +0200

    HTML Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+    return (
+        <div className=''>
+            <ul>
+                <li>Work with us</li>
+                <li>How Hi Friend was built</li>
+                <li>justus@hifriend.com</li>
+                <li>Twitter</li>
+                <li>TikTok</li>
+            </ul>
+            <ul>
+                <li>© 2020 Hi Friend</li>
+                <li>Imprint</li>
+                <li>Press</li>
+                <li>Privacy Policy</li>
+                <li>Crafted with ❤️ in 🇪🇺</li>
+            </ul>
+        </div>
+    );
+};
