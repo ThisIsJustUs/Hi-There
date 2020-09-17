20efee12f02006d669ea6ef144fb13d0707ee654
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2020-09-17 21:34:42 +0200

    CSS Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
     return (
-        <div className=''>
-            <ul>
-                <li>Work with us</li>
-                <li>How Hi Friend was built</li>
-                <li>justus@hifriend.com</li>
-                <li>Twitter</li>
-                <li>TikTok</li>
+        <div className='bg-soft py-5 px-8'>
+            <ul className='font-display text-2xl'>
+                <li className='my-5'>Work with us</li>
+                <li className='my-5'>How Hi Friend was built</li>
+                <li className='my-5'>justus@hifriend.com</li>
+                <li className='my-5'>Twitter</li>
+                <li className='my-5'> TikTok</li>
             </ul>
-            <ul>
-                <li>© 2020 Hi Friend</li>
-                <li>Imprint</li>
-                <li>Press</li>
-                <li>Privacy Policy</li>
-                <li>Crafted with ❤️ in 🇪🇺</li>
+            <ul className='mt-10'>
+                <li className='my-3'>© 2020 Hi Friend</li>
+                <li className='my-3'>Imprint</li>
+                <li className='my-3'>Press</li>
+                <li className='my-3'>Privacy Policy</li>
+                <li className='my-3'>Crafted with ❤ in 🇪🇺</li>
             </ul>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,14 @@ module.exports = {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
         },
+        extend: {
+            colors: {
+                green1: '#507255',
+                green2: '#6EB257',
+                green3: '#4AAD52',
+                green4: '#488B49',
+                soft: '#F8FBEC',
+            },
+        },
     },
 };
